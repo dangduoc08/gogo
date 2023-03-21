f916ba36e4f9c55a0d4cd9eb259fa2f609bc9588
Đăng Được <duoc200894@gmail.com>
2023-03-22 01:41:36 +0700

    Add injected dependencies checking in dynamic module

--- core/fn.go
+++ core/fn.go
@@ -23,7 +23,19 @@ func genProviderKey(p Provider) string {
 
 func createStaticModuleFromDynamicModule(dynamicModule any, injectedProviders map[string]Provider) *Module {
 	dynamicModuleType := reflect.TypeOf(dynamicModule)
+	localArgsIndex := make(map[string]int)
+	localArgs := []reflect.Value{}
+	globalArgs := []reflect.Value{}
 	args := []reflect.Value{}
+
+	genError := func(dynamicModuleType reflect.Type, dynamicArgKey string, index int) error {
+		return fmt.Errorf(
+			"can't resolve argument of %v. Please make sure that the argument %v at index [%v] is available in the injected providers",
+			strings.Replace(dynamicModuleType.String(), ") *core.Module", ")", 1),
+			dynamicArgKey,
+			index,
+		)
+	}
 
 	// loop through each input parameter of the dynamic module
 	for i := 0; i < dynamicModuleType.NumIn(); i++ {
@@ -40,22 +52,36 @@ func createStaticModuleFromDynamicModule(dynamicModule any, injectedProviders ma
 		if injectedProviders[dynamicArgKey] != nil {
 
 			// if an injected provider exists, append it to the list of arguments
-			args = append(args, reflect.ValueOf(injectedProviders[dynamicArgKey]))
+			localArgs = append(localArgs, reflect.ValueOf(injectedProviders[dynamicArgKey]))
+			localArgsIndex[dynamicArgKey] = i
 		} else if globalProviders[dynamicArgKey] != nil {
 
 			// if an injected provider doesn't exist, check if a global provider with the same type exists
 			// if a global provider exists, append it to the list of arguments
-			args = append(args, reflect.ValueOf(globalProviders[dynamicArgKey]))
+			globalArgs = append(globalArgs, reflect.ValueOf(globalProviders[dynamicArgKey]))
 		} else {
-			panic(fmt.Errorf(
-				"can't resolve argument of %v. Please make sure that the argument %v at index [%v] is available in the injected providers",
-				strings.Replace(dynamicModuleType.String(), ") *core.Module", ")", 1),
-				dynamicArgKey,
-				i,
-			))
+			panic(genError(dynamicModuleType, dynamicArgKey, i))
 		}
 	}
 
+	args = append(args, append(localArgs, globalArgs...)...)
+
 	// call the dynamic module with the list of arguments and convert the result to a static module
-	return reflect.ValueOf(dynamicModule).Call(args)[0].Interface().(*Module)
+	staticModule := reflect.ValueOf(dynamicModule).Call(args)[0].Interface().(*Module)
+
+	// recheck inject dependencies
+	actualLocalProviderMap := make(map[string]bool)
+	for _, provider := range staticModule.providers {
+		actualLocalProviderMap[genProviderKey(provider)] = true
+	}
+
+	for _, localProviders := range localArgs {
+		dynamicArgKey := genProviderKey(localProviders.Interface().(Provider))
+
+		if !actualLocalProviderMap[dynamicArgKey] && globalProviders[dynamicArgKey] == nil {
+			panic(genError(dynamicModuleType, dynamicArgKey, localArgsIndex[dynamicArgKey]))
+		}
+	}
+
+	return staticModule
 }
