b26198deaa79006812bd807c7a1be1e6d3314725
Đăng Được <duoc200894@gmail.com>
2023-12-24 18:35:39 +0700

    Add FieldLevel alias

--- aliases.go
+++ aliases.go
@@ -18,16 +18,17 @@ type (
 	HTTPException = *exception.HTTPException
 
 	// decorators
-	Context   = *ctx.Context
-	Request   = *http.Request
-	Response  = http.ResponseWriter
-	Body      = ctx.Body
-	Form      = ctx.Form
-	File      = ctx.File
-	Query     = ctx.Query
-	Header    = ctx.Header
-	Param     = ctx.Param
-	WSPayload = ctx.WSPayload
-	Next      = ctx.Next
-	Redirect  = ctx.Redirect
+	Context    = *ctx.Context
+	Request    = *http.Request
+	Response   = http.ResponseWriter
+	Body       = ctx.Body
+	Form       = ctx.Form
+	File       = ctx.File
+	Query      = ctx.Query
+	Header     = ctx.Header
+	Param      = ctx.Param
+	WSPayload  = ctx.WSPayload
+	Next       = ctx.Next
+	Redirect   = ctx.Redirect
+	FieldLevel = ctx.FieldLevel
 )
